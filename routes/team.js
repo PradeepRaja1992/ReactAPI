e0ce7fe3b9a4153540f23d2c96adf1021ad313b8
DontReact <dinearul@gmail.com>
2018-07-03 07:25:41 +0000

    changes

--- routes/team.js
+++ routes/team.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
 
-router.route('/hello').get(function(request,res){
-    res.send('Hello world');
+console.log("in team route");
+router.route('/').get(function(req, res, next) {
+    console.log("in team route: hello");
+    res.send('Hello');
 });
 
 module.exports = router;
