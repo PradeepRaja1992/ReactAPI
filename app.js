5b201e1e83b20e16345a04fff6070ad320e7d43f
DontReact <dinearul@gmail.com>
2018-07-04 11:54:25 +0000

    changes

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ var eventRoute = require('./routes/events');
 var app =  express();
 console.log("in app js");
 
-mongoose.connect('mongodb://10.74.17.199:27107/Dontreact');
+//mongoose.connect('mongodb://localhost:27107/Dontreact');
+mongoose.connect('mongodb://10.74.17.199/Dontreact', function(err, db) {
+    if (err) {
+        console.log('Unable to connect to the server. Please start the server. Error:', err);
+    } else {
+        console.log('Connected to Server successfully!');
+    }
+});
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin","*");
